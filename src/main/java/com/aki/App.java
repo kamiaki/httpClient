170c7d71262974b174c1a6da8e19c89d64b44508
kamiaki <247557768@qq.com>
2021-08-03 18:03:52 +0800

    华云东方调用微信

--- src/main/java/com/aki/App.java
+++ src/main/java/com/aki/App.java
@@ -1,49 +1,79 @@
 package com.aki;
 
 import com.google.gson.Gson;
+import com.sun.org.apache.regexp.internal.RE;
+import lombok.SneakyThrows;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Hello world!
  */
 public class App {
-    //    public static final String url = "http://web.juhe.cn:8080/finance/stock/hs";//请求接口地址
+
+    public static final String appid = "wx501e543f56ab0199";
+    public static final String secret = "7d06e8448e66b9644dce65657a8d21b8";
+
     // 获取token
-    public static final String url0 = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx501e543f56ab0199&secret=ff9b90cae6a47ab31a11386da59c8092";
+    public static final String url_getToken = "https://api.weixin.qq.com/cgi-bin/token";
 
-    public static final String url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=44_LR3gMJJjOsednOXJJchsIbkDBp6o9v5PaJHnFlZKUMFrKouDpFIecE4405YImBqJp4NUwg2o7QDlADBCN8c2XUlCbPv4KVVns-IcIQY9fMGSAW17PPutWThMvMPxuNcH7-iJiWEB8vBtEF10IWJeAIASGT&next_openid=";//请求接口地址
-    public static final String url2 = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=44_LR3gMJJjOsednOXJJchsIbkDBp6o9v5PaJHnFlZKUMFrKouDpFIecE4405YImBqJp4NUwg2o7QDlADBCN8c2XUlCbPv4KVVns-IcIQY9fMGSAW17PPutWThMvMPxuNcH7-iJiWEB8vBtEF10IWJeAIASGT&openid=";
-    public static final String url3 = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=44_LR3gMJJjOsednOXJJchsIbkDBp6o9v5PaJHnFlZKUMFrKouDpFIecE4405YImBqJp4NUwg2o7QDlADBCN8c2XUlCbPv4KVVns-IcIQY9fMGSAW17PPutWThMvMPxuNcH7-iJiWEB8vBtEF10IWJeAIASGT";
+    public static final String url_userList = "https://api.weixin.qq.com/cgi-bin/user/get";
+    public static final String url_userInfo = "https://api.weixin.qq.com/cgi-bin/user/info";
+    public static final String url_sendMsg = "https://api.weixin.qq.com/cgi-bin/message/custom/send";
 
-//    public static void main(String[] args) {
-//        String s = HttpUtil.doGet(url, null, 10 * 1000);
-//        System.out.println(s);
-//        UserList userList = new Gson().fromJson(s, UserList.class);
-//        UserList.DataBean data = userList.getData();
-//        List<String> openid = data.getOpenid();
-//        System.out.println(openid);
-//
-//        openid.forEach(x -> {
-//            String ss = HttpUtil.doGet(url2 + x, null, 10 * 1000);
-//            String message = null;
-//            try {
-//                message =  new String(ss.getBytes("ISO-8859-1"), "UTF-8");;
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            User user = new Gson().fromJson(message, User.class);
-//            System.out.println(user);
-//        });
-//    }
+    public static final String url3 = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=";
 
-    // oVxDHw4gpAc8GILAZ2wxl7Tpvs3E 王小秋
-    //oVxDHw2KKN1FKYV22nuHAnnmVx70
+    public static String getAccessToken() {
+        String query = "grant_type=client_credential" + "&appid=" + appid + "&secret=" + secret;
+        String token = HttpUtil.doGet(url_getToken, query, 10 * 1000);
+        Map<String, String> map = new Gson().fromJson(token, Map.class);
+        String access_token = map.get("access_token");
+        return access_token;
+    }
 
+    public static UserList getUserList(String accessToken) {
+        String query = "access_token=" + accessToken;
+        String userListStr = HttpUtil.doGet(url_userList, query, 10 * 1000);
+        UserList userList = new Gson().fromJson(userListStr, UserList.class);
+        return userList;
+    }
+
+    @SneakyThrows
+    public static User getUser(String accessToken, String openid) {
+        String queryX = "access_token=" + accessToken + "&openid=" + openid;
+        String userInfo = HttpUtil.doGet(url_userInfo, queryX, 10 * 1000);
+        String message = new String(userInfo.getBytes("ISO-8859-1"), "UTF-8");
+        User user = new Gson().fromJson(message, User.class);
+        return user;
+    }
+
+    public static String sendMsg(String accessToken, String openid, String msg) {
+        // oVxDHw4gpAc8GILAZ2wxl7Tpvs3E 王小秋
+        Gson gson = new Gson();
+        Map map = new HashMap();
+        map.put("touser", openid);
+        map.put("msgtype", "text");
+        Map text = new HashMap();
+        text.put("content", msg);
+        map.put("text", text);
+        String json = gson.toJson(map);
+        String returnStr = HttpUtil.doPostJson(url_sendMsg + "?access_token=" + accessToken, json, 10 * 1000);
+        return returnStr;
+    }
+
+    @SneakyThrows
     public static void main(String[] args) {
-        String json = "{\"touser\":\"oVxDHw4gpAc8GILAZ2wxl7Tpvs3E\",\"msgtype\":\"text\",\"text\":{\"content\":\"aaa\"}}";
-        Map map = new Gson().fromJson(json, Map.class);
-        String s = HttpUtil.doPostJson(url3, json, 10 * 1000);
-        System.out.println(s);
+        String accessToken = getAccessToken();
+//        UserList userList = getUserList(accessToken);
+//        UserList.DataBean data = userList.getData();
+//        List<String> openids = data.getOpenid();
+//        for (String openid : openids) {
+//            User user = getUser(accessToken, openid);
+//            if ("王小秋".equals(user.getNickname())) System.out.println(user);
+//        }
+        String reStr = sendMsg(accessToken, "oVxDHw4gpAc8GILAZ2wxl7Tpvs3E", "你好!");
+        System.out.println(reStr);
     }
 }
